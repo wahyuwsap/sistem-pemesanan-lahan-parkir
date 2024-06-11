43ee0477729172049624b7d67c857985e729ce18
zakytaufiq <151290835+kytaufiq@users.noreply.github.com>
2024-06-11 21:38:30 +0700

    Update pemesanan lahan parkir.cpp

--- pemesanan lahan parkir.cpp
+++ pemesanan lahan parkir.cpp
@@ -1,1 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+
+class User {
+public:
+    char username[50];
+    char password[50];
+
+    User(const char* uname, const char* pwd) {
+        strcpy(username, uname);
+        strcpy(password, pwd);
+    }
+};
+
+
+class ParkingLot {
+public:
+    string location;
+    string time;
+    double price;
+    vector<bool> slots;
+
+    ParkingLot(const string& loc, const string& t, double p, int numSlots) 
+        : location(loc), time(t), price(p), slots(numSlots, false) {}
+};
+
+vector<ParkingLot> parkingLots; 
+
+void initializeParkingLots() {
+    
+    parkingLots.push_back(ParkingLot("Lokasi 1", "08:00 - 18:00", 5000.0, 10));
+    parkingLots.push_back(ParkingLot("Lokasi 2", "08:00 - 18:00", 6000.0, 15));
+    parkingLots.push_back(ParkingLot("Lokasi 3", "08:00 - 18:00", 7000.0, 20));
+    parkingLots.push_back(ParkingLot("Lokasi 4", "08:00 - 18:00", 8000.0, 25));
+    
+}
+
+
+
+
